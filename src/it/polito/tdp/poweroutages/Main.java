0136d72c18305c0c40d4cf6a9c7282fb27473860
Utente <Utente@DESKTOP-73HFT01>
2020-03-18 17:09:00 +0100

    FINALE

--- src/it/polito/tdp/poweroutages/Main.java
+++ src/it/polito/tdp/poweroutages/Main.java
@@ -12,7 +12,6 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("PowerOutages.fxml"));
 			BorderPane root = (BorderPane) loader.load();
 			PowerOutagesController controller=loader.getController();
